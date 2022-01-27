cca282c19c3cefa7c815fee77c66c6381e0e93db
Derek Ewell <derekewell@gmail.com>
2022-01-26 17:08:35 -0700

    Icons, Favorites, and Comments

---
+++ components/ContactComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card
+            wrapperStyle={{ margin: 20 }}
+            title="Contact Information">
+            <View>
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+            
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </View>
+        </Card>
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact;
