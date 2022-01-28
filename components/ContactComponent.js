fea3d1bed135946a8714b660215330af54ec79b7
Derek Ewell <derekewell@gmail.com>
2022-01-28 09:42:04 -0700

    Using Redux

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -15,17 +15,17 @@ class Contact extends Component {
         return (
             <ScrollView>
                 <Card
-            wrapperStyle={{ margin: 20 }}
-            title="Contact Information">
-            <View>
-                <Text>1 Nucamp Way</Text>
-                <Text>Seattle, WA 98001</Text>
-                <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+                    wrapperStyle={{ margin: 20 }}
+                    title="Contact Information">
+                    <View>
+                        <Text>1 Nucamp Way</Text>
+                        <Text>Seattle, WA 98001</Text>
+                        <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
             
-                <Text>Phone: 1-206-555-1234</Text>
-                <Text>Email: campsites@nucamp.co</Text>
-            </View>
-        </Card>
+                        <Text>Phone: 1-206-555-1234</Text>
+                        <Text>Email: campsites@nucamp.co</Text>
+                    </View>
+                </Card>
             </ScrollView>
         );
     }
